3285fa87ccb1b90738a33eb6a63bb525baf385a2
Antonio <amcbd89@gmail.com>
2020-01-26 11:53:43 +0000

    Update views

--- graph/views.py
+++ graph/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index")
+    template = loader.get_template('graph/index.html')
+    return HttpResponse(template.render({}, request))
 
 def graph(request):
-    return HttpResponse("Graph")
+    template = loader.get_template('graph/graph.html')
+    return HttpResponse(template.render({}, request))
