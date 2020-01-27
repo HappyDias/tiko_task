564aef21aebaf2637f227451ae0ec6ec06bc41b7
Antonio <amcbd89@gmail.com>
2020-01-27 17:25:51 +0000

    Minor changes to the static code to test the watcher

--- static/functions.js
+++ static/functions.js
@@ -1,6 +1,6 @@
 var Chart = window.Chart
 
-export function draw_chart(arg){
+function draw_chart(arg){
 	var node = document.getElementById(arg);
 	if(!(Chart === undefined)){
 
@@ -28,12 +28,12 @@ export function draw_chart(arg){
 	throw {message: "Chart library does not exist"}
 }
 
-export function update_chart(chart, data){
+function update_chart(chart, data){
 	chart.data.datasets[0].data.push(data)
 	chart.update();
 }
 
-export function request_data(chart){
+function request_data(chart){
 	fetch('http://api.icndb.com/jokes/random')
 		.then( response => {
 			if(!response.ok){ throw response }
