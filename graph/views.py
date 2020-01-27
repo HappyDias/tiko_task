d9d26dfda65effded1bf072d1987ef6fa6cc7c44
Antonio <amcbd89@gmail.com>
2020-01-27 14:29:11 +0000

    Added a login_required decorator that redirects users to the admin login page when they are not properly authenticated

--- graph/views.py
+++ graph/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/admin/login/')
 def index(request):
     template = loader.get_template('graph/index.html')
     return HttpResponse(template.render({}, request))
 
+@login_required(login_url='/admin/login/')
 def graph(request):
     template = loader.get_template('graph/graph.html')
     return HttpResponse(template.render({}, request))
